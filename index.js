9ad0bdb83604a41ed1c3f381d3904a3a1aa4196d
jonathasgouv <51249397+jonathasgouv@users.noreply.github.com>
2020-12-27 16:26:26 -0300

    Add files via upload
    first commit

---
+++ index.js
@@ -0,0 +1,64 @@
+function nofmembers(element) {
+	number = element.value;
+	document.getElementById('tobesort').innerText = `Sortear entre os primeiros: ${number} membros`;
+}
+
+function nofmembers2(element) {
+	number = element.value;
+	document.getElementById('tobesort2').innerText = `Sortear: ${number} membros`;
+}
+
+function getRandom(arr, n) {
+	var result = new Array(n),
+		len = arr.length,
+		taken = new Array(len);
+	if (n > len) throw new RangeError('getRandom: more elements taken than available');
+	while (n--) {
+		var x = Math.floor(Math.random() * len);
+		result[n] = arr[x in taken ? taken[x] : x];
+		taken[x] = --len in taken ? taken[len] : len;
+	}
+	return result;
+}
+
+async function sort() {
+  document.getElementById('winners').innerHTML = '<div class="loader"></div>'
+	numberofmembers = document.getElementById('myRange').value;
+	numberofmemberstobesorted = document.getElementById('myRange2').value;
+	topic = document.getElementById('basic-url').value.split('_')[1].split('?')[0];
+	url = `https://api.sorteadorvk.repl.co/getMembers?tid=${topic}`;
+
+	data = await fetch(url);
+  data = await data.json();
+
+
+	members = data.uids.slice(1, numberofmembers);
+	winners = getRandom(members, numberofmemberstobesorted);
+
+	generateHTML(winners);
+}
+
+async function generateHTML(winners) {
+	html = '';
+	count = 0;
+
+	for (winner of winners) {
+		count++;
+		url = `https://api.sorteadorvk.repl.co/getMember?uid=${winner}`;
+		data = await fetch(url);
+    data =  await data.json();
+		name = data.response[0].first_name + ' ' + data.response[0].last_name;
+		avatar = data.response[0].photo_big;
+		href = `https://vk.com/id${winner}`;
+
+		html += `<div class="well well-lg">
+			<img src="${avatar}" alt="" srcset="">
+				<div class="name-position">
+					<a href="${href}">${name}</a>
+					<p>${count}º vencedor</p>
+				</div>
+			</div>  `;
+	}
+
+	document.getElementById('winners').innerHTML = html;
+}
